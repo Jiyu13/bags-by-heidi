3d7dfb3cd0df0b2352dd5a5ed85e59d271aef34e
jiyu13 <jiyucoding@yahoo.com>
2026-03-16 18:11:15 -0400

    create mobile menu feature

---
+++ src/components/global/MobileMenuBtn.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import menu_icon from "../../assets/icons/menu_24dp.svg";
+
+
+export default function MobileMenuBtn({isOpenMenu, handleOpenHeaderMenu}) {
+     return (
+         <MobileMenuContainer onClick={handleOpenHeaderMenu}>
+            <img
+                src={menu_icon}
+                alt="mobile menu hamburger"
+            />
+        </MobileMenuContainer>
+     )
+
+}
+
+const MobileMenuContainer  = styled.div`
+  width: 24px;
+  height: 24px;
+  padding: 0.5rem;
+  border-radius: 45px;
+  cursor: pointer;
+  position: fixed;
+  right: 20px;
+  top: 2rem;
+  transform: translateY(0);
+  transition: transform .35s cubic-bezier(.645, .045, .355, 1);
+  transition-delay: .35s;
+`
