182dccafef5bdfbdbf51b7436c8c2e2aa2356375
jiyu13 <jiyucoding@yahoo.com>
2026-03-18 09:33:29 -0400

    edit css

--- frontend/src/pages/BagDetailPage.jsx
+++ frontend/src/pages/BagDetailPage.jsx
@@ -30,7 +30,7 @@ export default function BagDetailPage() {
     return (
         <DetailPageContainer className='product-detail-page'>
             <DetailPageWrapper $isMobile={isMobile}>
-                <ImagesColumn>
+                <ImagesColumn >
                     <ProductImagesSection
                         coverImage={productDetail?.image}
                         otherImages={productDetail?.product_images}
@@ -38,13 +38,13 @@ export default function BagDetailPage() {
                 </ImagesColumn>
 
                 {/* =================== Detail ================*/}
-                <DetailSection $isMobile={isMobile}>
+                <DetailSection $isMobile={isMobile} className='detail-section'>
                     <ProductTitle>{productDetail?.title}</ProductTitle>
                     <p>
                         description: The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.
                     </p>
 
-                    <DescriptionContainer style={{borderTop: "1px solid #eaeaea"}}>
+                    <DescriptionContainer className='description-container'>
                         <ul style={{paddingLeft: "20px"}}>
                             <DescriptionItem>Can put material here</DescriptionItem>
                             <DescriptionItem>Bag dimensions</DescriptionItem>
@@ -64,25 +64,24 @@ const DetailPageContainer = styled.div`
   box-sizing: border-box;
   margin: 0 auto;
   max-width: 1440px;
-  //padding: 0 15px;      
-  padding: 0 1.5rem 1.85rem;
-
+  padding: 2rem 5rem 2.5rem;
   transition: margin-top 2s ease-in-out .5s;
   
-  @media (min-width: 750px) {
-      padding: 2rem 5rem 2.5rem;
+  @media (max-width: 868px) {
+    padding: 0 1.5rem 1.85rem;
   }
+  
 `
 
 const DetailPageWrapper = styled.div`
     display: flex;
   align-items: flex-start;
+  justify-content: center;
   flex-direction: ${({ $isMobile }) => ($isMobile ? "column" : "row")};
 `
 const ImagesColumn = styled.div`
-  width: 65%;
-
-  @media (max-width: 988px) {
+  width: 50%;
+  @media (max-width: 868px) {
     width: 100%;
   }
 `;
@@ -90,16 +89,16 @@ const ImagesColumn = styled.div`
 const DetailSection = styled.div`
   width: 35%;
   padding-left: 1.8rem;
-
   position: sticky;
   top: 8rem;
   align-self: flex-start;
 
-  @media (max-width: 988px) {
+  @media (max-width: 868px) {
     width: 100%;
     padding-left: 0;
     position: static;
     top: auto;
+    align-self: center;
   }
 `;
 
