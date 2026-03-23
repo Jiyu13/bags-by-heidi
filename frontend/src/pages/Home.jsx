c0358f212d9d9aeef878f3120d6f903e8071a20f
jiyu13 <jiyucoding@yahoo.com>
2026-03-23 16:01:44 -0400

    - remove Featured - add HomeCategories

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import HomeBanner from "../components/homepage/HomeBanner";
-import Featured from "../components/homepage/Featured";
 import BrandStory from "../components/homepage/BrandStory";
 import CustomerFeed from "../components/homepage/CustomerFeed";
 import {HomeCategories} from "../components/homepage/HomeCategories";
@@ -8,7 +7,7 @@ export default function Home() {
     return (
         <>
             <HomeBanner />
-            <Featured />
+            <HomeCategories />
             <BrandStory/>
             <CustomerFeed />
         </>
