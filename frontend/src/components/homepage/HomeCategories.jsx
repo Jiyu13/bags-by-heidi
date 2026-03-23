69251604e807302a95dd2394a14af45ba54f8d44
jiyu13 <jiyucoding@yahoo.com>
2026-03-23 16:01:11 -0400

    adjust category layout to fit 5 items

--- frontend/src/components/homepage/HomeCategories.jsx
+++ frontend/src/components/homepage/HomeCategories.jsx
@@ -9,58 +9,43 @@ export function HomeCategories() {
 
 
     return (
-        <CategoryContainer className='category-container'>
-            {productCategories?.map((category, index) => {
-                const category_name = category?.category
-                const lowercase = category_name.toLowerCase()
-                const cover_image = category?.cover_image
-                return (
-                    <CategoryItem className={lowercase} href={`/shop/${lowercase}`} key={category?.id}>
-                        <Img src={cover_image} alt={lowercase}/>
-                        <ShopButton>
-                            <ShopButtonLink>{category_name}</ShopButtonLink>
-                        </ShopButton>
-                    </CategoryItem>
-                )
-            })}
+        <CategorySectionContainer className='category-section-container'>
+            <CategorySectionWrapper className='category-wrapper-container'>
+                <CategoryListContainer className='category-list-container'>
+                     {productCategories?.map((category, index) => {
+                        const category_name = category?.category
+                        const lowercase = category_name.toLowerCase()
+                        const cover_image = category?.cover_image
+                        return (
+                            <CategoryItem className={lowercase} href={`/shop/${lowercase}`} key={category?.id}>
+                                <Img src={cover_image} alt={lowercase}/>
+                                <ShopButton>
+                                    <ShopButtonLink>{category_name}</ShopButtonLink>
+                                </ShopButton>
+                            </CategoryItem>
+                        )
+                    })}
 
-            {/*<CategoryItem className="backpacks" href={'/shop/backpacks'}>*/}
-            {/*    <Img src={backpacks} alt="backpacks"/>*/}
-            {/*    <ShopButton>*/}
-            {/*        <ShopButtonLink href={'/shop/backpacks'}>Backpacks</ShopButtonLink>*/}
-            {/*    </ShopButton>*/}
-            {/*</CategoryItem>*/}
-
-            {/*<CategoryItem className="handbags" href={'/shop/handbags'}>*/}
-            {/*    <Img src={handbags} alt="handbags"/>*/}
-            {/*    <ShopButton>*/}
-            {/*        <ShopButtonLink href={'/shop/handbags'}>Handbags</ShopButtonLink>*/}
-            {/*    </ShopButton>*/}
-            {/*</CategoryItem>*/}
-
-
-            {/*<CategoryItem className="totes" href={'/shop/totes'}>*/}
-            {/*    <Img src={totes} alt="totes"/>*/}
-            {/*    <ShopButton>*/}
-            {/*        <ShopButtonLink href={'/shop/totes'}>Totes</ShopButtonLink>*/}
-            {/*    </ShopButton>*/}
-            {/*</CategoryItem >*/}
-
-
-            {/*<CategoryItem className="accessories" href={'/shop/accessories'}>*/}
-            {/*    <Img src={accessories} alt="accessories"/>*/}
-            {/*    <ShopButton>*/}
-            {/*        <ShopButtonLink href={'/shop/accessories'}>Fun Accessories</ShopButtonLink>*/}
-            {/*    </ShopButton>*/}
-            {/*</CategoryItem>*/}
-
-
-
-        </CategoryContainer>
+                </CategoryListContainer>
+            </CategorySectionWrapper>
+        </CategorySectionContainer>
     )
 }
 
-const CategoryContainer = styled.div`
+const CategorySectionContainer = styled.section`
+  background: transparent;
+`;
+
+const CategorySectionWrapper = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 3rem 5rem;
+
+  @media (max-width: 988px) {
+    padding: 2.5rem 1.5rem;
+  }
+`;
+const CategoryListContainer = styled.div`
   display: grid;
   grid-gap: 0.8rem;
   margin: 0 auto;
@@ -69,70 +54,147 @@ const CategoryContainer = styled.div`
   
   @media only screen and (max-width: 768px) {
     grid-template-columns: repeat(8, 1fr);
-    grid-template-rows: repeat(8, 10vw);
-
+    grid-template-rows: repeat(16, 8vw);
+    
     .backpacks {
-      grid-column: 1 / 5;
-      grid-row: 1 / 5;
+        grid-column-start: 1;
+        grid-column-end: 9;
+        grid-row-start: 1;
+        grid-row-end: 9;
     }
 
     .handbags {
-      grid-column: 5 / 9;
-      grid-row: 1 / 5;
+      grid-column: 1 / 5;
+      grid-row: 9 / 13;
     }
 
     .totes {
+      grid-column: 5 / 9;
+      grid-row: 9 / 13;
+    }
+
+    .quilts {
       grid-column: 1 / 5;
-      grid-row: 5 / 9;
+      grid-row: 13 / 17;
     }
 
     .accessories {
       grid-column: 5 / 9;
-      grid-row: 5 / 9;
+      grid-row: 13 / 17;
     }
   }
 
-  @media only screen and (min-width: 769px) and (max-width: 989px) {
+  @media only screen and (min-width: 768px) and (max-width: 990px) {
     grid-template-columns: repeat(8, 1fr);
-    grid-template-rows: repeat(2, 12vw);
-
+    grid-template-rows: repeat(4, 8vw);
+    padding: 0 2rem;
+    
     .backpacks {
-      grid-column: 1 / 3;
-      grid-row: 1 / 3;
+        grid-column-start: 1;
+        grid-column-end: 5;
+        grid-row-start: 1;
+        grid-row-end: 5;
     }
 
     .handbags {
-      grid-column: 3 / 5;
-      grid-row: 1 / 3;
-    }
-
-    .totes {
       grid-column: 5 / 7;
       grid-row: 1 / 3;
     }
 
-    .accessories {
+    .totes {
       grid-column: 7 / 9;
       grid-row: 1 / 3;
+    }
+
+    .quilts {
+      grid-column: 5 / 7;
+      grid-row: 3 / 5;
+    }
+
+    .accessories {
+      grid-column: 7 / 9;
+      grid-row: 3 / 5;
     }
   }
 
   @media only screen and (min-width: 990px) {
-    grid-template-columns: repeat(4, 1fr);
-    grid-auto-rows: 300px;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(4, 8vw);
+    padding: 0 2rem;
+    //grid-auto-rows: 300px;
+    .backpacks {
+        grid-column-start: 1;
+        grid-column-end: 5;
+        grid-row-start: 1;
+        grid-row-end: 5;
+    }
+    .handbags {
+        grid-column-start: 5;
+        grid-column-end: 7;
+        grid-row-start: 1;
+        grid-row-end: 3;
+    }
 
-    .backpacks,
-    .handbags,
-    .totes,
+    .totes {
+        grid-column-start: 7;
+        grid-column-end: 9;
+        grid-row-start: 1;
+        grid-row-end: 3;
+    }
+
+    .quilts {
+        grid-column-start: 5;
+        grid-column-end: 7;
+        grid-row-start: 3;
+        grid-row-end: 5;
+    }
+
     .accessories {
-      grid-column: span 1;
-      grid-row: span 1;
+        grid-column-start: 7;
+        grid-column-end: 9;
+        grid-row-start: 3;
+        grid-row-end: 5;
     }
   }
 
   @media only screen and (min-width: 1280px) {
-    grid-template-columns: repeat(4, 1fr);
-    grid-auto-rows: 350px;
+    padding: 0 5rem;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(4, 8vw);
+    .backpacks {
+        grid-column-start: 1;
+        grid-column-end: 5;
+        grid-row-start: 1;
+        grid-row-end: 5;
+    }
+    .handbags {
+        grid-column-start: 5;
+        grid-column-end: 7;
+        grid-row-start: 1;
+        grid-row-end: 3;
+    }
+
+    .totes {
+        grid-column-start: 7;
+        grid-column-end: 9;
+        grid-row-start: 1;
+        grid-row-end: 3;
+    }
+
+    .quilts {
+        grid-column-start: 5;
+        grid-column-end: 7;
+        grid-row-start: 3;
+        grid-row-end: 5;
+    }
+
+    .accessories {
+        grid-column-start: 7;
+        grid-column-end: 9;
+        grid-row-start: 3;
+        grid-row-end: 5;
+    }
+    
   }
 `
 const CategoryItem = styled.a`
@@ -159,7 +221,7 @@ const ShopButtonLink = styled.div`
     padding: 12px 0;
     margin: 0 auto;
     cursor: pointer;
-    font-size: 0.9rem;
+    font-size: 1.1rem;
     text-align: center;
     text-decoration: none;
     
